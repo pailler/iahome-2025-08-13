183c9f13c569444aae9277f0e285733f6ae9fcbb
pailler <pailler@example.com>
2025-08-05 01:06:10 +0200

    Sauvegarde : 5 aout 2025

---
+++ git-commit.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Ajout des fichiers...
+git add .
+echo.
+echo Commit avec le message "Sauvegarde : 5 aout 2025"...
+git commit -m "Sauvegarde : 5 aout 2025"
+echo.
+echo Commit termine !
+pause 
